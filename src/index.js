d0cf48f8b2ef1a586f136efc842a5c8cb9f1815b
avfernandez03 <167803504+avfernandez03@users.noreply.github.com>
2024-05-30 18:16:14 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -19,8 +19,12 @@ let instructionsInput = document.querySelector("#user-instructions");
 
 let apiKey="tee233ebaaa4783bff41331b36o08895";
 let prompt=`Generate short Spanish bedtime story for kids about ${instructionsInput.value}`;
-let context=`You are a bedstory expert. Please provide short bedtime story in Spanish for small kids and use emoticons to replace a few words in the story.Make sure to follow the user instructions. Use the ${instructionsInput.value} inside a <strong> element`;
+let context=`You are a bedstory expert. Please provide short bedtime story in Spanish for small kids and use emoticons to replace a few words in the story.Make sure to follow the user instructions. Use the ${instructionsInput.value} inside a <strong> element every time it appears in the story`;
 let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+let storyElement = document.querySelector("#story");
+storyElement.classList.remove("hidden");
+storyElement.innerHTML = `<div class="generating"> ⏳ Generating Spanish bedtime story about "${instructionsInput.value}"</div>`
 
 axios.get(apiUrl).then(displayStory);    
 }
